d80f1b1207fa7c8596b4adc3a3e25295a862278c
Satin Jain <satinjain4@gmail.com>
2021-09-26 12:58:07 -0400

    Added Layout Files

--- gameMaker/src/main/java/Group3/gameMaker/Application/GameMaker.java
+++ gameMaker/src/main/java/Group3/gameMaker/Application/GameMaker.java
@@ -6,15 +6,62 @@
 //   Last change: Created GameMaker.java 
 
 
-
-
-
 package Group3.gameMaker.Application;
-public class GameMaker
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+
+
+public class GameMaker extends Application
 {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+//			Stage appStage;
+//			LayoutManager layoutManager;
+//			
+//			appStage = stage;
+//	    	layoutManager = new LayoutManager(appStage);
+//	    	layoutManager.makeStage();
+//	    	
+//	    	//Canvas gameCanvas = layoutManager.getGameCanvas();
+//			Scene gameScene = layoutManager.getGameScene();
+//			
+//			layoutManager.createButtons();
+//			layoutManager.showStage();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
     public static void main(String[] args)
     {
         //placeholder for now
         System.out.println("This is where we will launch the application.");
+        launch(args);
     }
 }
---
+++ gameMaker/src/main/java/Group3/gameMaker/CreateGameView/LayoutManager.java
@@ -0,0 +1,125 @@
+
+//    Created by: Snehal Patare
+//    Created on: 25 Sep 2021
+//     Edited by:
+//Edited last by: Snehal Patare
+
+package Group3.gameMaker.CreateGameView;
+
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.Pane;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+
+
+
+
+
+public class LayoutManager implements Layable {
+	
+	// Main Window
+	public Stage appStage;
+	public Group rootGroup;
+	public Pane rootPane;
+	
+	public LayablePane controlPane;
+	public Scene gameScene;
+	// Add list of items to be added in Layable array
+	public ArrayList<Layable> layables = new ArrayList<Layable>();
+	
+	public LayoutManager(Stage appStage) {
+		this.appStage = appStage;
+		appStage.setWidth(Location.RightLayout.rootPaneWidth);
+		appStage.setWidth(Location.RightLayout.rootPaneHeight);
+	}
+	
+	public void makeStage () {
+		// Build root group and root pane
+				rootGroup = new Group();
+				rootPane = new Pane();
+				rootPane.setPrefSize(Location.RightLayout.rootPaneWidth, Location.RightLayout.rootPaneHeight);
+				rootPane.setStyle("-fx-background-color: #FF00FF");
+				rootGroup.getChildren().add(rootPane);
+				
+//				gameScene = new Scene(rootGroup);
+//				// Not sure if this is right...
+//				appStage.setScene(gameScene);
+				
+				controlPane = new LayablePane();
+				layables.add(controlPane);
+				controlPane.setPrefSize(Location.TopLayout.controlPaneWidth, Location.TopLayout.controlPaneHeight);
+				rootGroup.getChildren().add(controlPane);
+				controlPane.setLayoutX(Location.TopLayout.controlPaneX);
+				controlPane.setStyle("-fx-background-color: #000000");
+				
+				gameScene = new Scene(rootGroup);
+				// Not sure if this is right...
+				appStage.setScene(gameScene);
+	}
+	
+	
+	public void createButtons () {
+		 
+		    
+		 // create a text input dialog
+//	        TextInputDialog td = new TextInputDialog("Enter X co-ordinates");
+//	        TextInputDialog td1 = new TextInputDialog("Enter Y co-ordinates");
+//	        // setHeaderText
+//	        td.setHeaderText("enter your name");
+//	        
+//	        final Popup popup = new Popup(); popup.setX(300); popup.setY(200);
+//		    popup.getContent().add(td,td1);
+//	  
+		
+		LayableButton button = new LayableButton("Circle");
+//		button.setOnAction(value -> {new EventHandler<ActionEvent>() {
+//		      @Override public void handle(ActionEvent event) {
+//		          popup.show(Dialogue);
+//		        }
+//		      });
+		button = new LayableButton("Rectangle");
+		button.setOnAction(value -> {
+//			gameEngine.reset();
+		});
+		addButtonToControlPanel(button);
+
+		button = new LayableButton("Triangle");
+		button.setOnAction(value -> {
+//			gameEngine.pause();
+		});
+		addButtonToControlPanel(button);
+        
+		
+	}
+	public void showStage() {
+		appStage.show();
+	}
+	
+	public void addButtonToControlPanel(LayableButton button) {
+		controlPane.AddChild(button);
+	}
+	
+	public Scene getGameScene() {
+		return gameScene;
+	}
+	
+	@Override
+	public void addLayable(Layable layable) {
+		// TODO Auto-generated method stub
+		layables.add(layable);
+	}
+
+	@Override
+	public void removeLayable(Layable layable) {
+		// TODO Auto-generated method stub
+		layables.remove(layable);
+		
+	}
+
+}
